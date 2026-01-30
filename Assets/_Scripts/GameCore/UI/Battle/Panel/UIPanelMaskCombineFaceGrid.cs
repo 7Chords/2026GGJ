10dafce82c8dc2abb660956da25e7945d2ee26c5
四不和声 <1223736337@qq.com>
2026-01-30 23:47:58 +0800

    dev：完善格子panel

---
+++ Assets/_Scripts/GameCore/UI/Battle/Panel/UIPanelMaskCombineFaceGrid.cs
@@ -0,0 +1,30 @@
+using SCFrame.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore.UI
+{
+    public class UIPanelMaskCombineFaceGrid : _ASCUIPanelBase<UIMonoMaskCombineFaceGrid>
+    {
+        public UIPanelMaskCombineFaceGrid(UIMonoMaskCombineFaceGrid _mono, SCUIShowType _showType) : base(_mono, _showType)
+        {
+        }
+
+        public override void AfterInitialize()
+        {
+        }
+
+        public override void BeforeDiscard()
+        {
+        }
+
+        public override void OnHidePanel()
+        {
+        }
+
+        public override void OnShowPanel()
+        {
+        }
+    }
+}
