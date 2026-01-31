fc4a877afc0fe1f1156483c76efce80562c90afa
四不和声 <1223736337@qq.com>
2026-01-31 23:31:24 +0800

    dev：优化商店物品item形式

--- Assets/_Scripts/GameCore/UI/Store/Mono/UIMonoStoreItem.cs
+++ Assets/_Scripts/GameCore/UI/Store/Mono/UIMonoStoreItem.cs
@@ -10,17 +10,23 @@ namespace GameCore.UI
     {
         [Header("商品图标")]
         public Image imgIcon;
-        [Header("商品价格")]
-        public Text txtPrice;
-        [Header("商品数量")]
-        public Text txtAmount;
+        [Header("部位价格")]
+        public Text txtPartPrice;
+        [Header("部位生命值")]
+        public Text txtPartHealth;
+        [Header("回血价格")]
+        public Text txtHealthPrice;
         [Header("购买按钮")]
         public Button btnPurchase;
-/*        [Header("商品名字")]
-        public Text txtName;*/
         [Header("鼠标移入的缩放")]
         public float scaleMouseEnter;
         [Header("鼠标移入的缩放时间")]
         public float scaleChgDuration;
+        [Header("已购买之后的画布阿尔法")]
+        public float hasPurchaseAlpha;
+        [Header("是部位货物显示的物品列表")]
+        public List<GameObject> goIsPartShowList;
+        [Header("是回血货物显示的物品列表")]
+        public List<GameObject> goIsHealthShowList;
     }
 }
