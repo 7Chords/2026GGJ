0dd6bbca717b5e97c17cfe2062e227c97a80f723
四不和声 <1223736337@qq.com>
2026-01-31 11:19:31 +0800

    dev：battleWin节点

--- Assets/_Scripts/GameCore/UI/Battle/Node/UINodeBattleWin.cs
+++ Assets/_Scripts/GameCore/UI/Battle/Node/UINodeBattleWin.cs
@@ -1,3 +1,4 @@
+using SCFrame;
 using SCFrame.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,48 +12,65 @@ namespace GameCore.UI
         {
         }
 
-        public override bool needHideWhenEnterNewSameTypeNode => throw new System.NotImplementedException();
+        public override bool needHideWhenEnterNewSameTypeNode => false;
 
-        public override bool needShowWhenQuitNewSameTypeNode => throw new System.NotImplementedException();
+        public override bool needShowWhenQuitNewSameTypeNode => false;
 
-        public override bool canQuitByEsc => throw new System.NotImplementedException();
+        public override bool canQuitByEsc => false;
 
-        public override bool canQuitByMouseRight => throw new System.NotImplementedException();
+        public override bool canQuitByMouseRight => false;
 
-        public override bool ignoreOnUIList => throw new System.NotImplementedException();
+        public override bool ignoreOnUIList => false;
 
-        public override SCUINodeFuncType nodeFuncType => throw new System.NotImplementedException();
+        public override SCUINodeFuncType nodeFuncType => SCUINodeFuncType.BATTLE;
 
-        public override bool needMoveToBottomWhenHide => throw new System.NotImplementedException();
+        public override bool needMoveToBottomWhenHide => false;
 
+        private GameObject _m_panelGO;
+        private UIPanelBattleWin _m_battleWinBtnPanel;
+        private UIMonoBattleWin _m_battleWinBtnMono;
         public override string GetNodeName()
         {
-            throw new System.NotImplementedException();
+            return nameof(UINodeBattleWin);
         }
 
         public override string GetResName()
         {
-            throw new System.NotImplementedException();
+            return "panel_battle_win";
         }
 
         public override void OnEnterNode()
         {
-            throw new System.NotImplementedException();
+            _m_panelGO = ResourcesHelper.LoadGameObject(GetResName(), GetRootTransform(), true);
+            if (_m_panelGO == null)
+            {
+                Debug.LogError("未找到资源名为" + GetResName() + "的资源!!!");
+                return;
+            }
+            _m_battleWinBtnMono = _m_panelGO.GetComponent<UIMonoBattleWin>();
+            if (_m_battleWinBtnMono == null)
+            {
+                Debug.LogError("资源名为" + GetResName() + "的资源上不存在对应的Mono!!!");
+                return;
+            }
+
+            _m_battleWinBtnPanel = new UIPanelBattleWin(_m_battleWinBtnMono, _m_showType);
+            _m_battleWinBtnPanel.Initialize();
         }
 
         public override void OnHideNode()
         {
-            throw new System.NotImplementedException();
+            _m_battleWinBtnPanel?.HidePanel();
         }
 
         public override void OnQuitNode()
         {
-            throw new System.NotImplementedException();
+            _m_battleWinBtnPanel?.Discard();
         }
 
         public override void OnShowNode()
         {
-            throw new System.NotImplementedException();
+            _m_battleWinBtnPanel?.ShowPanel();
         }
     }
 }
