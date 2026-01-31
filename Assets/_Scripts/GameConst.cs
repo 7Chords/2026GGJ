5273e7850f6f4986024e1a0d3e9a006a31a157ec
四不和声 <1223736337@qq.com>
2026-01-31 20:38:17 +0800

    dev

---
+++ Assets/_Scripts/GameConst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class GameConst
+    {
+        public static Color BTN_HIGHLIGHT_COLOR = new Color(0, 1, 0.0573f, 1);
+    }
+}
