a4fa529bfc9eb7faa5457cef2a0bc9d8acc5661c
四不和声 <1223736337@qq.com>
2026-02-12 20:37:57 +0800

    11

--- Assets/_Scripts/RefObj/EffectObj/EntryEffectObj.cs
+++ Assets/_Scripts/RefObj/EffectObj/EntryEffectObj.cs
@@ -9,15 +9,18 @@ namespace GameCore.RefData
     {
         public EAttributeTriggerPointType triggerPointType;
         public EAttributeType attributeType;
+        public float attributeChance;
+
         public float attributeValue;
         protected override void OnDeserialize(string _str)
         {
             string[] strArr = _str.Split(':');
-            if (strArr == null || strArr.Length < 3)
+            if (strArr == null || strArr.Length < 4)
                 return;
             triggerPointType = (EAttributeTriggerPointType)SCCommon.ParseEnum(strArr[0], typeof(EAttributeTriggerPointType));
             attributeType = (EAttributeType)SCCommon.ParseEnum(strArr[1],typeof(EAttributeType));
-            attributeValue = SCCommon.ParseFloat(strArr[2]);
+            attributeChance = SCCommon.ParseFloat(strArr[2]);
+            attributeValue = SCCommon.ParseFloat(strArr[3]);
         }
 
         protected override string OnSerialise()
