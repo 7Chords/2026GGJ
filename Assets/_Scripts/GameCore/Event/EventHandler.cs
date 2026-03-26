239dd12bf6c8ac4e6d67dbf940d0ef44240ca139
四不和声 <1223736337@qq.com>
2026-03-26 16:49:30 +0800

    dev:新事件

--- Assets/_Scripts/GameCore/Event/EventHandler.cs
+++ Assets/_Scripts/GameCore/Event/EventHandler.cs
@@ -15,17 +15,53 @@ namespace GameCore
                     break;
                 case EEventType.BLOOD_2_PART_HIGH:
                     {
+                        EventBlood2PartRefObj blood2PartRefObj = SCRefDataMgr.instance.eventBlood2PartRefList.refDataList.
+                            Find(x => x.floor == GameModel.instance.playerInfo.playerFloor && x.eventType == EEventType.BLOOD_2_PART_HIGH);
+                        if (blood2PartRefObj == null)
+                            return;
+                        long levelId = blood2PartRefObj.partList[Random.Range(0, blood2PartRefObj.partList.Count)];
+                        PartLevelRefObj levelRefObj = SCRefDataMgr.instance.partLevelRefList.refDataList.Find(x => x.id == levelId);
+                        if (levelRefObj == null)
+                            return;
+                        PartRefObj partRefObj = SCRefDataMgr.instance.partRefList.refDataList.Find(x => x.id == levelRefObj.partId);
+                        GameModel.instance.playerInfo.bagPartInfoList.Add(new PartInfo(partRefObj, false, levelRefObj.partLevel));
+                        GameCommon.ShowPopTip("获得" + partRefObj.partName, Vector2.zero);
 
+                        GameModel.instance.PlayerTakeDamage(blood2PartRefObj.blood);
                     }
                     break;
                 case EEventType.BLOOD_2_PART_MIDDLE:
                     {
+                        EventBlood2PartRefObj blood2PartRefObj = SCRefDataMgr.instance.eventBlood2PartRefList.refDataList.
+                            Find(x => x.floor == GameModel.instance.playerInfo.playerFloor && x.eventType == EEventType.BLOOD_2_PART_MIDDLE);
+                        if (blood2PartRefObj == null)
+                            return;
+                        long levelId = blood2PartRefObj.partList[Random.Range(0, blood2PartRefObj.partList.Count)];
+                        PartLevelRefObj levelRefObj = SCRefDataMgr.instance.partLevelRefList.refDataList.Find(x => x.id == levelId);
+                        if (levelRefObj == null)
+                            return;
+                        PartRefObj partRefObj = SCRefDataMgr.instance.partRefList.refDataList.Find(x => x.id == levelRefObj.partId);
+                        GameModel.instance.playerInfo.bagPartInfoList.Add(new PartInfo(partRefObj, false, levelRefObj.partLevel));
+                        GameCommon.ShowPopTip("获得" + partRefObj.partName, Vector2.zero);
 
+                        GameModel.instance.PlayerTakeDamage(blood2PartRefObj.blood);
                     }
                     break;
                 case EEventType.BLOOD_2_PART_LOW:
                     {
+                        EventBlood2PartRefObj blood2PartRefObj = SCRefDataMgr.instance.eventBlood2PartRefList.refDataList.
+                            Find(x => x.floor == GameModel.instance.playerInfo.playerFloor && x.eventType == EEventType.BLOOD_2_PART_LOW);
+                        if (blood2PartRefObj == null)
+                            return;
+                        long levelId = blood2PartRefObj.partList[Random.Range(0, blood2PartRefObj.partList.Count)];
+                        PartLevelRefObj levelRefObj = SCRefDataMgr.instance.partLevelRefList.refDataList.Find(x => x.id == levelId);
+                        if (levelRefObj == null)
+                            return;
+                        PartRefObj partRefObj = SCRefDataMgr.instance.partRefList.refDataList.Find(x => x.id == levelRefObj.partId);
+                        GameModel.instance.playerInfo.bagPartInfoList.Add(new PartInfo(partRefObj, false, levelRefObj.partLevel));
+                        GameCommon.ShowPopTip("获得" + partRefObj.partName, Vector2.zero);
 
+                        GameModel.instance.PlayerTakeDamage(blood2PartRefObj.blood);
                     }
                     break;
                 case EEventType.PART_2_PART:
